00273f435b9d0802dd7b9522389ef2be28e75cc4
dinesh0430 <dinesh.0430@gmail.com>
2020-02-27 23:26:21 +0000

    Add files via upload

---
+++ change.js
@@ -0,0 +1,63 @@
+function f1(opt) {
+  switch (opt) {
+    case 1:
+      document.getElementById("iframe-show").src = "home.html"
+      break
+    case 2:
+      document.getElementById("iframe-show").src = "login.html"
+      break
+    case 3:
+      document.getElementById("iframe-show").src = "register.html"
+      break
+    case 4:
+      document.getElementById("iframe-show").src = "company.html"
+      break
+    case 5:
+      document.getElementById("iframe-show").src = "alumni.html"
+      break
+    case 6:
+      document.getElementById("iframe-show").src = "placement-staff.html"
+      break
+    case 7:
+      document.getElementById("iframe-show").src = "student-details.html"
+      break
+    case 8:
+      document.getElementById("iframe-show").src = "academic-info.html"
+      break
+    case 9:
+      document.getElementById("iframe-show").src = "aggregate.html"
+      break
+  }
+}
+
+function f2(op) {
+  switch (op) {
+    case 1:
+      window.location.assign("aws.html")
+      break
+    case 2:
+      window.location.assign("byju.html")
+      break
+    case 3:
+      window.location.assign("capgemini.html")
+      break
+    case 4:
+      window.location.assign("microsoft.html")
+      break
+    case 5:
+      window.location.assign("tcs.html")
+      break
+    case 6:
+      window.location.assign("genpact.html")
+      break
+    case 7:
+      window.location.assign("accenture.html")
+      break
+    case 8:
+      window.location.assign("ibm.html")
+      break
+    case 9:
+      window.location.assign("hcl.html")
+      break
+  }
+}
